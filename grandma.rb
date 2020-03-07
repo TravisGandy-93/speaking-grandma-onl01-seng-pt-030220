1afba6592c2e4a3fccc6d7968bf13130a2b9fd7e
Travis Gandy <travis_gandy@yahoo.com>
2020-03-07 06:44:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(love)
   if love == "i love you grandma!".upcase
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif love == 
-    
+    puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
  
